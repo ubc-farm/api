04a83cb7357f1b365edb5760e69c07954a64731b
Tiger Oakes <tigeroakes@gmail.com>
2016-05-24 13:36:57 -0700

    Converted utils to a ES6 module

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,52 +1,29 @@
-/** @file Creates farm namespace and utility scripts */
+/**
+ * Resolves when the DOM is ready to interact with
+ * @type {Promise<void>}
+ * @author Jake Archibald
+ * @see {@link https://github.com/jakearchibald/offline-wikipedia}
+ */
+export const domReady = new Promise(resolve => {
+	function checkState() {
+		if (document.readyState != 'loading') resolve();
+	}
+	document.addEventListener('readystatechange', checkState);
+	checkState();
+})
 
 /**
- * Namespace for UBC Farm scripts
- * @namespace
+ * Creates a promise that can be resolved from the outside.
+ * Used for async scripts and their onload attributes.
+ * @property {Promise<any>} promise
+ * @property {function} resolve - resolves the promise
+ * @property {function} reject - rejects the promise
  */
-var farm = {
-	/**
-	 * Namespace for functions that create elements
-	 * @namespace
-	 */
-	element: {},
-	
-	/**
-	 * Namespace for functions that create page views
-	 * @namespace
-	 */
-	page: {},
-	
-	/**
-	 * Namespace for utlity functions
-	 * @namespace
-	 */
-	utils: {
-		/**
-		 * Resolves when the DOM is ready to interact with
-		 * @type {Promise<void>}
-		 * @author Jake Archibald
-		 * @see {@link https://github.com/jakearchibald/offline-wikipedia}
-		 */
-		domReady: new Promise(function(resolve) {
-			function checkState() {
-				if (document.readyState != 'loading') resolve();
-			}
-			document.addEventListener('readystatechange', checkState);
-			checkState();
-		}),
-		
-		/**
-		 * Creates a promise that can be resolved from the outside.
-		 * Used for async scripts and their onload attributes.
-		 * @class
-		 * @return {{promise: Promise<any>, resolve: function, reject: function}}
-		 */
-		Deferred: function() {
-			this.promise = new Promise(function(resolve, reject) {
-				this.reject = reject;
-				this.resolve = resolve;
-			})
-		}
+export class Deferred {
+	constructor() {
+		this.promise = new Promise((resolve, reject) => {
+			this.resolve = resolve;
+			this.reject = reject;
+		})
 	}
 }
