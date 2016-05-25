71461f470b632c9a0192ad5be63a94dd59b5da81
Tiger Oakes <tigeroakes@gmail.com>
2016-05-25 14:59:45 -0700

    Tweaked generator to use querystring and to optionally have no key

--- views/_helpers/google_maps.js
+++ views/_helpers/google_maps.js
@@ -1,16 +1,20 @@
+const querystring = require('querystring');
 const src = 'https://maps.googleapis.com/maps/api/js';
-const {GOOGLE_TOKEN: key=''} = process.env;
+const {GOOGLE_TOKEN: key} = process.env;
 
 /**
  * Creates a google maps API link
  * @param {string|string[]} [input.libraries] - list of libraries to use
  */
-module.exports = function(libraries) {
-	let library = '';
+module.exports = function(...libraries) {
+	let q = {};
+	
+	if (key) q.key = key;
 	if (libraries) {
 		if (!Array.isArray(libraries)) libraries = [libraries];
-		library = `&libraries=${libraries.join(',')}`;
+
+		q.libraries = libraries.join(',')
 	}
-	
-	return `"${src}?key=${key}${library}"`;
+
+	return `${src}?${querystring.stringify(q)}`;
 }
