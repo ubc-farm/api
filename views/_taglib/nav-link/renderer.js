942a28998c392675dce5fbc3eeabebe87c51a655
Tiger Oakes <tigeroakes@gmail.com>
2016-05-25 10:13:42 -0700

    Added sidebar code. Current page floats to the top as a header.

--- views/_taglib/nav-link/renderer.js
+++ views/_taglib/nav-link/renderer.js
@@ -3,18 +3,23 @@
  * @param {string} input.api - string corresponding to apis' properties
  * @param {string} [input.href] - link to use. If ommited, one is generated from the name
  * @param {string|boolean} [input.active] - if true, or if the text is equal to the name (case insensitive), add the class active to this link
+ * @param {string} [input.prefix=/] prefix for link generation
+ * @param {boolean} [input.color] sets up link for colorized hover
  */
 exports.render = (input, out) => {
-	let {name, href, active = false, class: cname=''} = input;
+	let {name, href, active=false, prefix='/', color} = input;
 	
-	if (!href) {href = `/${name.toLowerCase()}`}
+	if (!href) {href = prefix + name.toLowerCase()}
 	if (typeof active === 'string') {
 		active = (active.toLowerCase() === name.toLowerCase())
 	}
 	
 	out.write('<a class="nav-link ')
-	if (active) {out.write(' active ')}
-	out.write(cname);
+	if (active) {out.write('nav-active ')}
+	if (color) {
+		out.write('nav-color" data-name="')
+		out.write(name)
+	}
 	out.write('" href="')
 	out.write(href) 
 	out.write('">')
