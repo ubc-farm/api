c9feceaad420cc474c8302be17eabb36868a204a
Tiger Oakes <tigeroakes@gmail.com>
2016-05-26 10:46:30 -0700

    Switched to bable plugin that only transpiles modules instead of all of es6

--- frontend/sys-config.js
+++ frontend/sys-config.js
@@ -6,10 +6,16 @@ var googleMeta = {
 var c = {
 	baseURL: '/js',
 	map: {
-		traceur: '/js/vendor/traceur.js'
+		traceur: '/js/vendor/traceur.js',
+		'plugin-babel': '/js/vendor/plugin-babel.js',
+		'systemjs-babel-build': '/js/vendor/systemjs-babel-browser.js'
+	},
+	transpiler: 'plugin-babel',
+	babelOptions: {
+		es2015: false
 	},
 	meta: {
 		'google-maps': googleMeta,
-		'google-maps-drawing': googleMeta
+		'google-maps-drawing': googleMeta,
 	}
 }
