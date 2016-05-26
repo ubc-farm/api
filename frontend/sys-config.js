94b7a94203fcd395b4bf2959f179b382d7933807
Tiger Oakes <tigeroakes@gmail.com>
2016-05-26 09:57:17 -0700

    Created a config script file for System.config that can be cached

---
+++ frontend/sys-config.js
@@ -0,0 +1,15 @@
+var googleMeta = {
+	scriptLoad: true,
+	format: 'global',
+	exports: 'google'
+};
+var c = {
+	baseURL: '/js',
+	map: {
+		traceur: '/js/vendor/traceur.js'
+	},
+	meta: {
+		'google-maps': googleMeta,
+		'google-maps-drawing': googleMeta
+	}
+}
