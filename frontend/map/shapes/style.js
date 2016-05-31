f28003ccbb90a866f1cce1ee39a9e003a8ca2b9b
Tiger Oakes <tigeroakes@gmail.com>
2016-05-31 14:26:04 -0700

    Seperated styles into seperate exports

--- frontend/map/shapes/style.js
+++ frontend/map/shapes/style.js
@@ -7,41 +7,46 @@ export const color = {
 	light: '#ddd'
 }
 
-export const styles = {
-	grid: {
+export const grid = {
+	normal: {
 		zIndex: 2,
 		fillOpacity: 0.0,
 		strokeOpacity: 0.5,
 		fillColor: color.gray,
 		strokeColor: color.light
 	},
-	'grid-hover': {
+	hover: {
 		fillOpacity: 0.4
 	},
-	'grid-selected': {
+	selected: {
 		fillOpacity: 0.5,
 		strokeOpacity: 0.9,
 		fillColor: color.accent,
 		strokeColor: color.accent
-	},
-	field: {
-		editable: true,
+	}
+}
+
+export const field = {
+	normal: {
 		fillOpacity: 0.5,
 		strokeOpacity: 1,
 		fillColor: color.primary,
 		strokeColor: color.primary
-	},
-	edge: {
+	}
+}
+
+export const edge = {
+	normal: {
 		strokeOpacity: 0.0,
 		strokeColor: color.accent,
 		strokeWeight: 6,
 		zIndex: 3,
 		visible: false
 	},
-	'edge-hover': {
+	hover: {
 		strokeOpacity: 1.0
 	},
-	'edge-active': {
+	show: {
 		visible: true
 	}
 }
