1cc66caf1c46cf39edafcb67d0330e5cf7fc6e06
Tiger Oakes <tigeroakes@gmail.com>
2016-05-31 10:50:54 -0700

    Updated Grid cells to extend Polygons

--- backend/geo/grid/cell.js
+++ backend/geo/grid/cell.js
@@ -2,13 +2,19 @@ const {offset} = require('../latlng/spherical.js');
 const Angle = require('../latlng/angle.js');
 const {geom} = require('jsts');
 
-module.exports = class GridCell {
+module.exports = class GridCell extends geom.Polygon {
 	constructor(start, width, height, angle) {
-		this.start = new geom.Coordinate(start);
+		this.start = new geom.Coordinate(start.x, start.y);
 		this.width = width;
 		this.height = height;
 		this.parallel = Angle.parse(angle);
 		this.perpendicular = Angle.rotate(this.parallel, -90);
+		
+		/** Initiate geom.Polygon */
+		let factory = new geom.GeometryFactory();
+		super(factory.createLinearRing(
+			buildPath().map(p => new geom.Coordinate(p.x, p.y))
+		), [], factory);
 	}
 	
 	/**
@@ -16,7 +22,7 @@ module.exports = class GridCell {
 	 * point of the cell. 
 	 * @returns {LatLng[]} 
 	 */
-	get path() {
+	buildPath() {
 		if (this._path) return this._path;
 		//Build path clockwise
 		let point1 = offset(this.start, this.height, this.perpendicular);
@@ -24,9 +30,8 @@ module.exports = class GridCell {
 		let point3 = offset(point2, this.height, this.perpendicular * -1);
 		
 		this._path = [this.start, point1, point2, point3];
+		return this._path;
 	}
 	
-	within(container) {
-		
-	}
+	//intersects(parent)
 }
