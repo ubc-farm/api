337155dfabedbfc8a31ea371b6913628adbfa7c8
Tiger Oakes <tigeroakes@gmail.com>
2016-05-31 09:56:25 -0700

    Added force method

--- backend/geo/latlng/set.js
+++ backend/geo/latlng/set.js
@@ -26,6 +26,18 @@ module.exports = class LatLngSet extends Map {
 	}
 	
 	/**
+	 * Adds without double-checking that an item isn't in the set.
+	 * Useful if you already checked Set.has() beforehand.
+	 * @param {LatLng} value - item to add
+	 * @returns {LatLngSet} this set
+	 */
+	forceAdd(value) {
+		value = LatLng.parse(value);
+		super.set(value.toString(), value);
+		return this;
+	}
+	
+	/**
 	 * Equivalent to Set.delete(). Removes a value
 	 * from the set.
 	 * @param {LatLng} value - item to delete
