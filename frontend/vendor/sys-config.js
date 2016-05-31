553b576edada45e618ff587393be724fc2ee50cf
Tiger Oakes <tigeroakes@gmail.com>
2016-05-31 13:22:26 -0700

    Setting up systemJS for jsts

--- frontend/vendor/sys-config.js
+++ frontend/vendor/sys-config.js
@@ -7,10 +7,14 @@ System.config({
 	baseURL: '/js',
 	map: {
 		'google/maps': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU',
-		'google/maps/edit': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=geometry,drawing'
+		'google/maps/drawing': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=drawing',
+		'google/maps/edit': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=geometry,drawing',
+		jsts: '/js/vendor/jsts.js'
 	},
 	meta: {
 		'google/maps': googleMeta,
+		'google/maps/drawing': googleMeta,
 		'google/maps/edit': googleMeta,
+		jsts: {format: 'global', exports: 'jsts'}
 	}
 })
--- frontend/workers/index.js
+++ frontend/workers/index.js
@@ -1,1 +1,6 @@
 importScripts('../vendor/system.js', '../vendor/sys-config.js');
+onmessage = function(e) {
+	System.import(e.data)
+		.then(() => {console.log("Loaded worker", e.data)})
+		.catch(error => {console.error(error)})
+}
