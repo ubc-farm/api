b8d3fae94f7a1f71b1c98981a6d1cb7301e927e4
Tiger Oakes <tigeroakes@gmail.com>
2016-06-07 14:57:44 -0700

    Worked on sale relations

--- backend/database/models/sale.js
+++ backend/database/models/sale.js
@@ -1,8 +1,31 @@
 import {Model} from 'objection';
 
+import Person from './person';
 import Location from './ref/location.js';
+
 export default class Sale extends Model {
 	static get tableName() {return 'Sale'}
+
+	static get relationMappings() {
+		return {
+			customer: {
+				relation: Model.OneToOneRelation,
+				modelClass: Person,
+				join: {
+					from: 'Sale.customerId',
+					to: 'Person.id'
+				}
+			},
+			deliveryLoc: {
+				relation: Model.OneToOneRelation,
+				modelClass: Location,
+				join: {
+					from: 'Sale.deliveryLocation',
+					to: 'Location.id'
+				}
+			}
+		}
+	}
 }
 
 export class Grant extends Sale {
