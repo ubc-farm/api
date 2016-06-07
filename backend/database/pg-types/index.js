f6143321f0888a416c899a8c1bb7c1200480e4c7
Tiger Oakes <tigeroakes@gmail.com>
2016-06-07 15:42:37 -0700

    Added datetime parser

--- backend/database/pg-types/index.js
+++ backend/database/pg-types/index.js
@@ -2,6 +2,7 @@ import {types} from 'pg';
 import {oid as PG} from './oid.js';
 
 import {point, path, circle} from './parse/geometric.js';
+import {timestamp, time, interval} from './parse/datetime.js';
 
 types.setTypeParser(PG.point, point);
 types.setTypeParser(PG.lseg, path);
@@ -9,3 +10,10 @@ types.setTypeParser(PG.box, path);
 types.setTypeParser(PG.path, path);
 types.setTypeParser(PG.polygon, polygon);
 types.setTypeParser(PG.circle, circle);
+
+types.setTypeParser(PG.timestamp, timestamp);
+types.setTypeParser(PG.timestamptz, timestamp);
+types.setTypeParser(PG.date, timestamp);
+types.setTypeParser(PG.time, time);
+types.setTypeParser(PG.timetz, time);
+types.setTypeParser(PG.interval, interval);
