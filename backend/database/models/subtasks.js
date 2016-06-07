fbb5b622769e3b0e0980290d37e5de044ed3455c
Tiger Oakes <tigeroakes@gmail.com>
2016-06-07 11:27:11 -0700

    Moved out crop-related subtasks

--- backend/database/models/subtasks.js
+++ backend/database/models/subtasks.js
@@ -2,41 +2,7 @@ import {Model} from 'objection';
 import Task from './task.js';
 
 import {Person as Company} from './person.js';
-import {Crop} from './field.js';
-import {Plant, Item, Chemical} from './reference.js';
-
-export class Seeding extends Task {
-	static get tableName() {return 'Seeding'}
-
-	static get relationMappings() {
-		return Object.assign({
-			seeded: {
-				relation: Model.OneToOneRelation,
-				modelClass: Crop,
-				join: {
-					from: 'Seeding.crop',
-					to: 'Crop.id'
-				}
-			},
-			seedVariety: {
-				relation: Model.OneToOneRelation,
-				modelClass: Plant,
-				join: {
-					from: 'Seeding.variety',
-					to: 'Plant.id'
-				}
-			},
-			seedProduct: {
-				relation: Model.OneToOneRelation,
-				modelClass: Item,
-				join: {
-					from: 'Seeding.product',
-					to: 'Item.id'
-				}
-			}
-		}, super.relationMappings);
-	}
-}
+import {Chemical} from './reference.js';
 
 export class Irrigation extends Task {
 	static get tableName() {return 'Irrigation'}
@@ -53,40 +19,6 @@ export class PestControl extends Task {
 				join: {
 					from: 'PestControl.product',
 					to: 'Chemical.id'
-				}
-			}
-		}, super.relationMappings);
-	}
-}
-
-export class ScoutHarvest extends Task {
-	static get tableName() {return 'ScoutHarvest'}
-
-	static get relationMappings() {
-		return Object.assign({
-			crop: {
-				relation: Model.OneToOneRelation,
-				modelClass: Crop,
-				join: {
-					from: 'ScoutHarvest.cropId',
-					to: 'Crop.id'
-				}
-			}
-		}, super.relationMappings);
-	}
-}
-
-export class ScoutPest extends Task {
-	static get tableName() {return 'ScoutPest'}
-
-	static get relationMappings() {
-		return Object.assign({
-			crop: {
-				relation: Model.OneToOneRelation,
-				modelClass: Crop,
-				join: {
-					from: 'ScoutPest.cropId',
-					to: 'Crop.id'
 				}
 			}
 		}, super.relationMappings);
