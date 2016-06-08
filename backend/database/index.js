2c9a32207a38b18b812aa8cc9dcb996d27abeea0
Tiger Oakes <tigeroakes@gmail.com>
2016-06-08 10:38:35 -0700

    Exporting type helper, exporting models as a single module

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,5 +1,9 @@
+import initTypes from './pg-types';
 import objection from 'objection';
 import Knex from 'knex';
+import * as model from './models';
+
+initTypes();
 
 const knex = Knex({
 	client: 'pg',
