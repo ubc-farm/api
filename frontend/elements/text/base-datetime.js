79528317a61e4c086af537be90a06f890b132a86
Tiger Oakes <tigeroakes@gmail.com>
2016-06-08 15:19:25 -0700

    Adding datetime visual code

---
+++ frontend/elements/text/base-datetime.js
@@ -0,0 +1,59 @@
+/**
+ * Used to render a time. You can either pass in a date object or pass in
+ * a time string in format HHMM (such as 1300 for 13 o'clock / 1 pm).
+ * @param {Date|string} date object or a string
+ * @param {boolean} [amPm=true] - set to false to prevent AM/PM from showing
+ * @param {boolean} [trailing=false] - set to false to truncate hours, 
+ * i.e.: 12:00 becomes 12 instead. 
+ * @param {boolean} [twelve=true] - true to use 12 hours clock, false to use 24
+ */
+export default function TimeBase({date, amPm=true,trailing=false,twelve=true}) {
+	if (typeof date === 'string') {
+		let time = date.toString();
+		let hr = 0, min = parseInt(time.slice(-2));
+		if (time.length > 2) hr = parseInt(time.slice(0, -2));
+
+		date = new Date(0);
+		date.setHours(hr, min);
+	} else if (date == null) {
+		return '';
+	}
+
+	let hour = date.getHours(), minute = date.getMinutes(), amPmStr = '';
+	if (twelve) {
+		if (hour === 0) {
+			hour = 12;
+			if (amPm) amPmStr = ' AM';
+		} else if (amPm && hour === 12) {
+			amPmStr = ' PM';
+		} else if (hour > 12) {
+			hour = hour % 12;
+			if (amPm) amPmStr = ' PM';
+		} else if (amPm) { amPmStr = ' AM'; }
+	}
+	
+	let minString = ':' + minute.toString();
+	if (trailing && minute === 0) { minString = '';	}
+
+	return hour.toString() + minString + amPmStr;
+}
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+/**
+ * Used to render a date. You can pass a Date object or an object with the 
+ * correct properties.
+ * @param {Date|Object} date object or json object
+ * @param {number} time.date
+ * @param {number} time.month
+ * @param {number} time.year
+ * @param {boolean} [showYear=false] - wheter or not to display the year
+ * @param {boolean} [shortMonth=true] - wheter or not to shorten the month
+ */
+export default function DateBase({date, showYear = false, shortMonth = true}) {
+	if (!date instanceof Date) {
+		date = new Date(date.year, date.month, date.date);
+	}
+}
