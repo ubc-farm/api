2f0d1b7d9d712f1f555b356bb57ba4d9c1c9ff1f
Tiger Oakes <tigeroakes@gmail.com>
2016-06-08 11:36:25 -0700

    Adding react, removing unneeded jsts meta

--- frontend/vendor/sys-config.js
+++ frontend/vendor/sys-config.js
@@ -9,12 +9,13 @@ System.config({
 		'google/maps': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU',
 		'google/maps/drawing': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=drawing',
 		'google/maps/edit': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=geometry,drawing',
-		jsts: '/js/vendor/jsts.js'
+		jsts: '/js/vendor/jsts.js',
+		react: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js',
+		'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js'
 	},
 	meta: {
 		'google/maps': googleMeta,
 		'google/maps/drawing': googleMeta,
-		'google/maps/edit': googleMeta,
-		jsts: {format: 'global', exports: 'jsts'}
+		'google/maps/edit': googleMeta
 	}
 })
