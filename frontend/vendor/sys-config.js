8152f87879e0e3d3c55ac748d3d30f125dd2f2f7
Tiger Oakes <tigeroakes@gmail.com>
2016-06-10 13:44:24 -0700

    Added react-router to map

--- frontend/vendor/sys-config.js
+++ frontend/vendor/sys-config.js
@@ -11,7 +11,8 @@ System.config({
 		'google/maps/edit': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=geometry,drawing',
 		jsts: '/js/vendor/jsts.js',
 		react: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js',
-		'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js'
+		'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js',
+		'react-router': '/js/vendor/react-router'
 	},
 	meta: {
 		'google/maps': googleMeta,
