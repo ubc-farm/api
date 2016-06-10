a9a9edb9a3d0f9901cfeae541610454df9964985
Tiger Oakes <tigeroakes@gmail.com>
2016-06-10 14:38:01 -0700

    Added more elements

---
+++ frontend/elements/form/field.jsx
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+import _ from '../classnames.js';
+
+/**
+ * A text field for a single line of text
+ */
+export default class Field extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange() {
+		
+	}
+
+	render() {
+
+	}
+}
