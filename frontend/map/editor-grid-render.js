c9e74b8b5400eb6257b2da6827eb4dd7c14926f9
Tiger Oakes <tigeroakes@gmail.com>
2016-06-13 09:07:43 -0700

    Switched to functions rather than a class

--- frontend/map/editor-grid-render.js
+++ frontend/map/editor-grid-render.js
@@ -6,45 +6,43 @@ import * as style from 'map/shapes/style.js';
 import ModuleWorker from 'workers/promise/system.js';
 import GridSelector from 'map/shapes/select.js';
 
-export default class GridBuilder {
-	constructor(map, defaultGridOptions) {
-		this.map = map;
-		this.defaultGrid = defaultGridOptions || {
-			width: 2, height: 2,
-			angle: 25,
-			widthSpecific: [], heightSpecific: []
-		};
-		this.worker = new ModuleWorker('workers/grid.js');
-	}
+const defaultGrid = {
+	width: 2, height: 2,
+	angle: 25,
+	widthSpecific: [], heightSpecific: []
+};
 
-	/**
-	 * Display a grid on the selected polygon
-	 */
-	setActive(polygon, gridOptions) {
-		let gridOpts;
-		if (polygon.gridOptions) gridOpts = polygon.gridOptions;
-		else if (gridOptions) gridOpts = gridOptions;
+export function worker() {
+	return new ModuleWorker('workers/grid.js');
+}
 
-		let path = polygon.getPath().getArray().map(point => {
-			let {lng: x, lat: y} = point.toJSON();
-			return {x, y};
-		});
-		path.push(path[0]);
+/**
+ * Display a grid on the selected polygon
+ */
+export function setActive(polygon, gridOptions) {
+	let gridOpts;
+	if (polygon.gridOptions) gridOpts = polygon.gridOptions;
+	else if (gridOptions) gridOpts = gridOptions;
 
-		return this.buildGridData(path, gridOpts).then(grid => {
-			this.map.data.add(grid);
-		})
-	}
+	let path = polygon.getPath().getArray().map(point => {
+		let {lng: x, lat: y} = point.toJSON();
+		return {x, y};
+	});
+	path.push(path[0]);
 
-	/**
-	 * Process the path within a web worker
-	 */
-	buildGridData(path, gridSpec) {
-		gridSpec = gridSpec || this.defaultGrid;
-		return this.worker.postMessage({
-			name: JSON.stringify(path), //@todo create ID for polygon
-			path, 
-			gridSpec
-		})
-	}
+	let map = polygon.getMap();
+	return buildGridData(path, gridOpts).then(grid => {
+		map.data.add(grid);
+	})
+}
+
+/**
+ * Process the path within a web worker
+ */
+export function buildGridData(path, gridSpec = defaultGrid, worker = worker()) {
+	return worker.postMessage({
+		name: JSON.stringify(path), //@todo create ID for polygon
+		path, 
+		gridSpec
+	})
 }
