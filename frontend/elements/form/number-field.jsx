e59f9034c97a01bcf6723f36e145d39b15237927
Tiger Oakes <tigeroakes@gmail.com>
2016-06-14 12:20:28 -0700

    Rewrote number field to render textfield instead of extending it, which is apparently the React way of inheritance

--- frontend/elements/form/number-field.jsx
+++ frontend/elements/form/number-field.jsx
@@ -1,32 +1,19 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import TextField from './text-field.js';
 
-export default class NumberField extends TextField {
-	render() {
-		return React.cloneElement(super.render(), Object.assign({
-			type: 'number'
-		}, this.props));
-	}
-
-	static get propTypes() {
-		let superProps = super.propTypes;
-		delete superProps.type;
-		delete superProps.maxLength;
-		let props = {
-			min: PropTypes.number,
-			max: PropTypes.number,
-			step: PropTypes.number
-		}
-		return Object.assign(props, superProps);
-	}
-
-	static get defaultProps() {
-		let superDefaults = super.defaultProps;
-		delete superDefaults.type;
-		delete superDefaults.maxLength;
-		let defaults = {
-			step: 'any'
-		}
-		return Object.assign(defaults, superDefaults);
-	}
+export default function NumberField(props) {
+	return <TextField {...props} type='number'/>
 }
+
+NumberField.propTypes = Object.assign({
+	min: PropTypes.number,
+	max: PropTypes.number,
+	step: PropTypes.oneOfType([
+		PropTypes.number,
+		PropTypes.oneOf(['any'])
+	]),
+}, TextField.propTypes);
+
+NumberField.defaultProps = Object.assign({
+	step: 'any'
+}, TextField.defaultProps)
