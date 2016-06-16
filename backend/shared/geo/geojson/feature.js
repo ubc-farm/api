6ee8486fd5bf7cd7a4fda0d917e7a807b5fe3c93
Tiger Oakes <tigeroakes@gmail.com>
2016-06-16 16:03:48 -0700

    Added feature

---
+++ backend/shared/geo/geojson/feature.js
@@ -0,0 +1,14 @@
+export default class Feature {
+	static get type() {return 'Feature'}
+	constructor(geometry, properties, id) {
+		Object.assign(this, {geometry, properties});
+		if (id) this.id = id;
+	}
+
+	toJSON() {
+		let {type, geometry, properties, id} = this;
+		let json = {type, geometry, properties};
+		if (id) json.id = id;
+		return json;
+	}
+}
