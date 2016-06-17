8e1b569bb4b91ff81b6cce711a7ce3549def827d
Tiger Oakes <tigeroakes@gmail.com>
2016-06-17 14:07:42 -0700

    Changed NavLink to use child node as name instead of a property

--- backend/shared/elements/nav/nav-link.jsx
+++ backend/shared/elements/nav/nav-link.jsx
@@ -3,7 +3,7 @@ import _ from '../classnames.js';
 
 /**
  * @param {Object} props
- * @param {string} props.name
+ * @param {string} props.children
  * @param {string} [props.href] - link to use. If ommited, one is generated from the name
  * @param {boolean} [props.active] - if true, add the class active to this link
  * @param {string} [props.prefix=/] prefix for link generation
@@ -12,7 +12,7 @@ import _ from '../classnames.js';
  */
 export default function NavLink(props) {
 	if (hide) return null;
-	let href = props.href || prefix + props.name.toLowerCase();
+	let href = props.href || prefix + props.children.toLowerCase();
 	return (
 		<a className={_(
 			'nav-link', 'hover-light',
@@ -20,13 +20,13 @@ export default function NavLink(props) {
 				'nav-active': props.active,
 				'nav-color': props.color
 			}
-		)} data-color={props.color && props.name} href={href}>
-			{props.name}
+		)} data-color={props.color && props.children} href={href}>
+			{props.children}
 		</a>
 	);
 }
 NavLink.propTypes = {
-	name: PropTypes.string.isRequired,
+	children: PropTypes.string.isRequired,
 	href: PropTypes.string,
 	prefix: PropTypes.string,
 	active: PropTypes.bool,
