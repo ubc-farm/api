169206c9e33f22b99167a71071c367e4ccf399f6
Tiger Oakes <tigeroakes@gmail.com>
2016-06-20 16:04:33 -0700

    Updated to pass route params

--- backend/server/routes/marko.js
+++ backend/server/routes/marko.js
@@ -1,5 +1,8 @@
 import * as path from 'path';
 
+/**
+ * Handler for marko files that rewrites their filenames
+ */
 function handler(request, reply) {
 	let {dir, name, ext} = path.posix.parse(request.path);
 	const filePath = path.format({dir, name, ext: '.marko'})
@@ -7,12 +10,12 @@ function handler(request, reply) {
 		//check if filename.marko exists, otherwise use filename/index.marko
 		exists(filePath).then(doesExist => {
 			if (doesExist) {
-				return reply.view(filePath);
+				return reply.view(filePath, request.params);
 			} else {
-				return reply.view(path.join(dir, name, 'index.marko'));
+				return reply.view(path.join(dir, name, 'index.marko'), request.params);
 			}
 		})
-	} else return reply.view(filePath);
+	} else return reply.view(filePath, request.params);
 }
 
 /** Routes for marko views */
@@ -21,6 +24,21 @@ const routes = [
 		method: 'GET',
 		path: '/fields/edit/{clientParams*}',
 		handler
-	}
+	},
+	{
+		method: 'GET',
+		path: '/fields/{fieldID?}',
+		handler
+	},
+	{
+		method: 'GET',
+		path: '/events/{task?}',
+		handler
+	},
+	{
+		method: 'GET',
+		path: '/finances/{page*}',
+		handler
+	},
 ];
 export default routes;
