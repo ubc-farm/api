bd5739bbde2d7784ec860bb1061ad20fdd74431e
Tiger Oakes <tigeroakes@gmail.com>
2016-06-21 13:53:34 -0700

    Added firebase modules

--- frontend/vendor/sys-config.js
+++ frontend/vendor/sys-config.js
@@ -1,22 +1,35 @@
-const googleMeta = {
-	scriptLoad: true,
-	format: 'global',
-	exports: 'google'
-};
-System.config({
-	baseURL: '/js',
-	map: {
-		'google/maps': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU',
-		'google/maps/drawing': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=drawing',
-		'google/maps/edit': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU&libraries=geometry,drawing',
+(function(System) {
+	function firebase(map, ...features) {
+		let config = {};
+		for (let feature of features) {
+			config['firebase/' + feature] = 
+				`https://www.gstatic.com/firebasejs/3.0.5/firebase-${feature}.js`;
+		}
+		return Object.assign(map, config);
+	}
+	const gMap = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARDOjzy7qB7QdqbO0i5Gt5q_ogVcTSdWU';
+	const map = {
+		'google/maps': gMap,
+		'google/maps/edit': gMap + '&libraries=geometry,drawing',
 		jsts: '/js/vendor/jsts.js',
 		react: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js',
-		'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js',
+		'react-dom': 
+			'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js',
 		'react-router': '/js/vendor/react-router'
-	},
-	meta: {
-		'google/maps': googleMeta,
-		'google/maps/drawing': googleMeta,
-		'google/maps/edit': googleMeta
 	}
-})
+
+	System.config({
+		baseURL: '/js',
+		map: firebase(map, 'app', 'auth', 'database', 'storage'),
+		meta: {
+			'google/maps/*': {
+				scriptLoad: true,
+				format: 'global',
+				exports: 'google'
+			},
+			'firebase/*': { 
+				format: 'global'
+			}
+		}
+	})
+})(System)
