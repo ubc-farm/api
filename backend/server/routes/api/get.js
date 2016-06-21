f5e77d9bcf90db8176198516e66b540a49ff40b2
Tiger Oakes <tigeroakes@gmail.com>
2016-06-21 16:36:37 -0700

    Working on API code with similar format to firebase

--- backend/server/routes/api/get.js
+++ backend/server/routes/api/get.js
@@ -1,14 +1,39 @@
-const route = {
-	method: 'GET',
-	path: `/api/${Model.tableName}/{id}/{path*}`,
-	handler: function(request, reply) {
-		const {print, shallow, orderBy} = request.query;
-		const path = request.params.path.split('/');
-		if (print === 'pretty') reply.spaces('/t');
+import modify from './modify.js'
 
-		
-	}, 
-	config: {
-		bind: Model
+function getAll(request, reply) {
+	const data = this.query()
+		.then(rows => {
+			let json = { [this.label]: {} };
+			for (let row of rows) json[this.label][row.$id] = row;
+			return json;
+		});
+	return modify(data, request, reply);
+}
+
+function get(request, reply) {
+	const {id, property} = request.params;
+	const query = this.query().findById(id)
+	if (property) query.column(property);
+	return modify(query.execute(), request, reply);
+}
+
+export default function get(model) {
+	if (!model.label) model.label = model.tableName.toLowerCase() + 's';
+	const route = {
+		method: 'GET',
+		config: {
+			bind: Model
+		}
 	}
+
+	return [
+		Object.assign(route, {
+			path: '/api/' + model.label + '.json',
+			handler: getAll
+		}),
+		Object.assign(route, {
+			path: '/api/' + model.label + '/{id}/{property?}.json',
+			handler: get
+		})
+	]
 }
