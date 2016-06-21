c5ef17cb02ff67132eb674fa9a552bd2893f90bb
Tiger Oakes <tigeroakes@gmail.com>
2016-06-21 09:12:40 -0700

    Turned jsx transform back on for server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("babel-register")({
 	plugins: [
 		'transform-strict-mode',
-		//'transform-react-jsx',
+		'transform-react-jsx',
 		'transform-es2015-modules-commonjs'
 	],
 	babelrc: false
