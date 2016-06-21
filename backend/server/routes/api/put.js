e2a3b306b4fa0a466289aeba2da78523828a5381
Tiger Oakes <tigeroakes@gmail.com>
2016-06-21 15:20:19 -0700

    Working on some API routes

---
+++ backend/server/routes/api/put.js
@@ -0,0 +1,14 @@
+const route = {
+	method: 'PUT',
+	path: `/api/${Model.tableName}/{id}/{path*}`,
+	handler: function(request, reply) {
+		const {print, shallow, orderBy} = request.query;
+		const path = request.params.path.split('/');
+		if (print === 'pretty') reply.spaces('/t');
+
+		
+	}, 
+	config: {
+		bind: Model
+	}
+}
