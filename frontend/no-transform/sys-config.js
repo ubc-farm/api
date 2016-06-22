7dcd7d3085604e40984b7a5472123f954c43851e
Tiger Oakes <tigeroakes@gmail.com>
2016-06-22 14:37:27 -0700

    Removed wildcard usage in system config file

--- frontend/no-transform/sys-config.js
+++ frontend/no-transform/sys-config.js
@@ -11,6 +11,7 @@
 	const map = {
 		'google/maps': gMap,
 		'google/maps/edit': gMap + '&libraries=geometry,drawing',
+		'google/maps/drawing': gMap + '&libraries=drawing',
 		jsts: '/js/vendor/jsts.js',
 		react: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js',
 		'react-dom': 
@@ -18,15 +19,19 @@
 		'react-router': '/js/vendor/react-router'
 	}
 
+	const gConfig = {
+		scriptLoad: true,
+		format: 'global',
+		exports: 'google'
+	}
+
 	System.config({
 		baseURL: '/js',
 		map: firebase(map, 'app', 'auth', 'database', 'storage'),
 		meta: {
-			'google/maps/*': {
-				scriptLoad: true,
-				format: 'global',
-				exports: 'google'
-			},
+			'google/maps': gConfig,
+			'google/maps/edit': gConfig,
+			'google/maps/drawing': gConfig,
 			'firebase/*': { 
 				format: 'global'
 			}
