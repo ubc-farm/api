2fbe275fc168e8caab5eb84be502b1ab6855ea29
Tiger Oakes <tigeroakes@gmail.com>
2016-06-22 10:49:35 -0700

    Moved to reflect no-transform addition

--- frontend/vendor/firebase.js
+++ frontend/vendor/firebase.js
@@ -1,7 +1,10 @@
-const firebase = {
+import firebase from 'firebase/app';
+
+const config = {
   apiKey: "AIzaSyAhtZflr4OJ3EmGhyh5OzSwMIChMgJdswc",
   authDomain: "hidden-outrider-132223.firebaseapp.com",
   databaseURL: "https://hidden-outrider-132223.firebaseio.com",
 	storageBucket: "hidden-outrider-132223.appspot.com",
 }
-export default firebase;
+
+export default firebase.initializeApp(config);
