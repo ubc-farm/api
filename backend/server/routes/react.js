95510b7580a966f45a66fd1bd90e1003b4171c9c
Tiger Oakes <tigeroakes@gmail.com>
2016-06-22 11:44:11 -0700

    Added test route to debug react views

---
+++ backend/server/routes/react.js
@@ -0,0 +1,18 @@
+import search from './search.js';
+
+/**
+ * Handler for marko files that rewrites their filenames
+ */
+function handler(request, reply) {
+	search(request, reply, '.jsx');
+}
+
+/** Routes for marko views */
+const routes = [
+	{
+		method: 'GET',
+		path: '/test',
+		handler
+	}
+];
+export default routes;
