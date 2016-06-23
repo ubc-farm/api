bb274c970cfaf899e2b3c080a0be637892f04262
Tiger Oakes <tigeroakes@gmail.com>
2016-06-23 16:36:12 -0700

    Updating editor code

---
+++ app/polygon-editor/googlemap.js
@@ -0,0 +1,34 @@
+/** @enum */
+const mode = {
+	ADD: 'add',
+	SELECT: 'select',
+	RESIZE: 'resize'
+}
+
+export class PolygonEditor {
+	constructor(node) {
+		this.worker = new ModuleWorker('lib/autogrid/worker');
+	}
+
+	mode(newMode) {
+		if (this._mode === newMode) return;
+		else if (newMode === mode.ADD) {
+			// let user draw on map
+			this.drawManager.setDrawingMode(google.maps.drawing.OverlayType.POLYGON 
+				|| 'polygon');
+		} else if (newMode === mode.SELECT) {
+			// let user click on map
+			this.drawManager.setDrawingMode(null);
+		} else if (newMode === mode.RESIZE) {
+			// make polygon resizable
+		}
+	}
+
+	focus(polygon, newSettings) {
+
+	}
+
+	merge(cells) {
+
+	}
+}
