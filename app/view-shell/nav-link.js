fe9ce9f71d6465f9ae08f9472d05b23a4a0ef3ef
Tiger Oakes <tigeroakes@gmail.com>
2016-06-24 09:21:38 -0700

    Tweaked link code

--- app/view-shell/nav-link.js
+++ app/view-shell/nav-link.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import { createElement as r, PropTypes } from 'react';
 import _ from '../classnames.js';
 
 /**
@@ -10,20 +10,14 @@ import _ from '../classnames.js';
  * @param {boolean} [props.color] sets up link for colorized hover
  * @param {boolean} [props.hide] hides if true 
  */
-export default function NavLink(props) {
+export default function NavLink({hide, href, prefix, children, active, color}) {
 	if (props.hide) return null;
-	const href = props.href || props.prefix + props.children.toLowerCase();
-	return (
-		<a className={_(
-			'nav-link', 'hover-light',
-			{
-				'nav-active': props.active,
-				'nav-color': props.color
-			}
-		)} data-name={props.color && props.children} href={href}>
-			{props.children}
-		</a>
-	);
+	const className = _('nav-link', 'hover-light', {
+		'nav-active': active, 'nav-color': color
+	});
+	href = href || prefix + children.toLowerCase();
+
+	return r('a', {className, href, 'data-name': color && children})
 }
 NavLink.propTypes = {
 	children: PropTypes.string.isRequired,
