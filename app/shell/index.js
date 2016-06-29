c5255528a5308f82156795b08e81f6cbe1db08e8
Tiger Oakes <tigeroakes@gmail.com>
2016-06-29 09:17:46 -0700

    Added shell export

---
+++ app/shell/index.js
@@ -0,0 +1,6 @@
+export {default} from './shell.js';
+export {default as HTML} from './html.js'
+
+export {default as Banner} from './banner.js';
+export {default as Sidebar} from './sidebar.js';
+export {default as NavLink} from './nav-link.js';
