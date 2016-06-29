e49f33672586ff948f8c2906429ac5d6c88b2b9c
Tiger Oakes <tigeroakes@gmail.com>
2016-06-29 11:35:12 -0700

    Set api route generator to use app/models

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -14,6 +14,7 @@ const labels = ['people', 'employees', 'researchers', 'assignments',
 	'equipment', 'equipmentusage', 'events', 'tasks', 'fields', 'crops', 'items', 
 	'plants', 'location', 'program', 'programusage', 'account', 'chemicals', 
 	'sales', 'grants', 'researchprojects', 'researchpartners', 'mixes']
+import * as models from 'app/models';
 
 function* methodRoutes(table, alias) {
 	for (let method in methods) {
@@ -34,16 +35,18 @@ function* methodRoutes(table, alias) {
 
 function* tableRoutes() {
 	for (let i = 0; i < tables.length; i++) {
-		yield methodRoutes(tables[i], labels[i]);
+		yield* methodRoutes(tables[i], labels[i]);
 	}
 }
 
 function* modelRoutes() {
-	for (let model in models) {
+	for (let modelName in models) {
+		const model = models[modelName];
+		console.log(modelName);
 		if (!model.label) model.label = model.tableName.toLowerCase() + 's';
-		yield methodRoutes(model, model.label);
+		yield* methodRoutes(model, model.label);
 	}
 }
 
-const routes = [...tableRoutes()];
+const routes = [...modelRoutes()];
 export default routes;
