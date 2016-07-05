8bc16ce70573fe91aaf07c9087c5dbd72c8d4fb8
Tiger Oakes <tigeroakes@gmail.com>
2016-07-05 14:19:34 -0700

    Added router JSX

--- app/shell/shell.js
+++ app/shell/shell.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from 'app/store';
 import Sidebar from './sidebar.js';
 import Banner from './banner.js';
 
-const reduxApp = (
-	<Provider store={store}>
-		<div>
-			<Sidebar/>
-			<Banner/>
-			<main className='main'>{children}</main>
-		</div>
-	</Provider>
+const App = (
+	<div>
+		<Sidebar/>
+		<Banner/>
+		<main className='main'>{children}</main>
+	</div>
 );
 
-export default reduxApp;
+export default App;
--- app/store.js
+++ app/store.js
@@ -1,13 +1,14 @@
 import {combineReducers, createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
+import {routerReducer as routing} from 'react-router-redux';
 import map from 'app/googlemap-connector/reducer';
 import navigation from 'app/navigation/reducer';
-//import subreducer from '';
 
 export const rootReducer = combineReducers({
 	map,
-	navigation
+	navigation,
+	routing
 })
 
 let store = createStore(
