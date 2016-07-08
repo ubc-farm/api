cb7413d3f2c134b2e184a9d3d245bce3e832c51c
Tiger Oakes <tigeroakes@gmail.com>
2016-07-08 09:19:24 -0700

    Added code for checkbox inputs

---
+++ app/checkbox/index.js
@@ -0,0 +1,6 @@
+/**
+ * @module app/checkbox
+ */
+
+export {default} from './checkbox.js';
+export {default as Radio} from './radio.js';
---
+++ app/checkbox/radio.js
@@ -0,0 +1,21 @@
+import React, {PropTypes} from 'react';
+import {classlist} from 'lib/utils';
+import Checkbox from './checkbox.js'
+
+/**
+ * Radio button based on the Checkbox code. Takes the same props as Checkbox.
+ * @param {Object} props
+ */
+const Radio = props => (
+	<label className={classlist(
+		'check-hack-label', 'radio-hack-label', props.className
+	)}>
+		<input {...props} className='check-real radio-real'/>
+		<i className='checkbox-hack radio-hack' type='radio'/>
+		{props.children}
+	</label>
+)
+
+Radio.propTypes = Checkbox.propTypes;
+
+export default Radio;
