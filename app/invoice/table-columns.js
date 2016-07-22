91dfa593e855ec43b6b732d378bf5fcce6c929e1
Tiger Oakes <tigeroakes@gmail.com>
2016-07-22 12:04:37 -0700

    Changed code so that inputs are generated in columns rather than the table code

--- app/invoice/table-columns.js
+++ app/invoice/table-columns.js
@@ -1,24 +1,18 @@
-import {createElement} from 'react';
+import React from 'react';
 import {Column, Cell} from '../../lib/table-controls/index.js';
 import Money from '../../lib/money/index.js';
 
-export const item = new Column({
+export const item = {
 	columnKey: 'item',
-	compareFunc: true,
-	type: 'text',
-	spellCheck: true,
-	placeholder: 'Squash, kg'
-});
+	compareFunc: true
+}
 
-export const description = new Column({
+export const description = {
 	columnKey: 'description',
-	compareFunc: true,
-	type: 'text',
-	spellCheck: true,
-	placeholder: 'Squash variety 2, kg'
-})
+	compareFunc: true
+}
 
-export const unitCost = new Column({
+export const unitCost = {
 	columnKey: 'unitCost',
 	title: 'Unit Cost ($)',
 	description: 'Cost per unit of this item',
@@ -29,21 +23,18 @@ export const unitCost = new Column({
 	compareFunc(a = 0, b = 0) {
 		return b - a;
 	},
-	placeholder: '',
-	align: 'right',
-	type: 'number',
-	step: 0.01
-})
+	align: 'right'
+}
 
-export const quantity = new Column({
+export const quantity = {
 	columnKey: 'quantity',
 	compareFunc: true,
 	align: 'right',
 	type: 'number',
 	step: 'any'
-})
+}
 
-export const price = new Column({
+export const price = {
 	columnKey: 'price',
 	title: 'Price ($)',
 	getValue(rowData) {
@@ -53,12 +44,64 @@ export const price = new Column({
 	compareFunc(a = 0, b = 0) {
 		return b - a;
 	},
-	toElement(money, props) {
-		const moneyString = money !== undefined ? 
-			money.toString() : undefined; 
-		return createElement(Cell, props, moneyString);
-	},
 	align: 'right'
-})
+}
 
-export default [item, description, unitCost, quantity, price];
+/** @returns {Column} copy of object with the specified extra properties */
+const clone = (from, ...extra) => new Column(Object.assign({}, from, ...extra))
+
+export default function invoiceColumns(onChangeCallback) {
+	return [
+		clone(item, { toElement(value, props, rowKey) {
+			const onChange = e => onChangeCallback(e, rowKey, item.columnKey);
+			return (
+				<Cell {...props} header scope='row'>
+					<input type='text' spellCheck
+						placeholder='Squash, kg'
+						value={value}
+						onChange={onChange}
+					/>
+				</Cell>
+			);
+		}}),
+		clone(description, { toElement(value, props, rowKey) {
+			const onChange = e => onChangeCallback(e, rowKey, description.columnKey);
+			return (
+				<Cell {...props}>
+					<input type='text' spellCheck
+						placeholder='Squash variety 2, kg'
+						value={value}
+						onChange={onChange}
+					/>
+				</Cell>
+			);
+		}}),
+		clone(unitCost, { toElement(value, props, rowKey) {
+			const onChange = e => onChangeCallback(e, rowKey, unitCost.columnKey);
+			return (
+				<Cell {...props}>
+					<input type='number' 
+						placeholder='2.99'
+						value={value}
+						onChange={onChange}
+						step={0.01}
+					/>
+				</Cell>
+			);
+		}}),
+		clone(quantity, { toElement(value, props, rowKey) {
+			const onChange = e => onChangeCallback(e, rowKey, quantity.columnKey);
+			return (
+				<Cell {...props}>
+					<input type='number' 
+						placeholder='2.99'
+						value={value}
+						onChange={onChange}
+						step={0.01}
+					/>
+				</Cell>
+			);
+		}}),
+		clone(price)
+	];
+}
