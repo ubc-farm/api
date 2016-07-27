aca5ed9ad3aaf5a7f47ad2d1e18f189715011abc
Tiger Oakes <tigeroakes@gmail.com>
2016-07-27 10:14:52 -0700

    Added node-resolve to rollup, removed old code

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	plugins: [
 		babel({
 			exclude: 'node_modules/**',
 			plugins: ['transform-react-jsx', 'external-helpers-2']
+		}),
+		nodeResolve({
+			jsnext: true,
+			main: true,
+			browser: true
 		})
 	],
 	sourceMap: true
