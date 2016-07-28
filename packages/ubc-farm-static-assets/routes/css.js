acda1a7079b4fe18cbe03b2c54fd6ec0f44833e8
Tiger Oakes <tigeroakes@gmail.com>
2016-07-28 15:24:39 -0700

    Moved analytics code to static assets, cleaned up using server.path

--- packages/ubc-farm-static-assets/routes/css.js
+++ packages/ubc-farm-static-assets/routes/css.js
@@ -1,13 +1,9 @@
-import {resolve} from 'path';
-const packageFolder = resolve(__dirname, '../../');
-const cssFolder = resolve(packageFolder, './ubc-farm-css');
-
 export const main = {
 	method: 'GET',
 	path: '/css/{param}',
 	handler: {
 		directory: {
-			path: cssFolder,
+			path: 'ubc-farm-css',
 			listing: true,
 			defaultExtension: 'css',
 			index: false
@@ -20,7 +16,7 @@ export const partialStyles = {
 	path: '/css/partials/{param}',
 	handler: {
 		directory: {
-			path: resolve(packageFolder, './ubc-farm-view-helpers/styles'),
+			path: 'ubc-farm-view-helpers/styles',
 			listing: true,
 			defaultExtension: 'css',
 			index: false
