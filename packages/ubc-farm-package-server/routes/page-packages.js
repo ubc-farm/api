5943405b06673b0040b6d1b28c19b572f83ccd33
Tiger Oakes <tigeroakes@gmail.com>
2016-07-28 14:09:01 -0700

    Added -page prefix to client side subpackages

--- packages/ubc-farm-package-server/routes/page-packages.js
+++ packages/ubc-farm-package-server/routes/page-packages.js
@@ -2,24 +2,24 @@ import {resolve, join} from 'path';
 
 const packageFolder = resolve(__dirname, '../../');
 
-import directoryConfig from '../../ubc-farm-directory/rollup.config.js';
+import directoryConfig from '../../ubc-farm-page-directory/rollup.config.js';
 export const directory = {
 	method: 'GET',
-	path: '/packages/ubc-farm-directory{ext?}',
+	path: '/packages/ubc-farm-page-directory{ext?}',
 	handler: {
 		package: Object.assign({}, directoryConfig, {
-			entry: join(packageFolder, 'ubc-farm-directory/index.js')
+			entry: join(packageFolder, 'ubc-farm-page-directory/index.js')
 		})
 	}
 }
 
-import invoiceConfig from '../../ubc-farm-invoice/rollup.config.js';
+import invoiceConfig from '../../ubc-farm-page-invoice/rollup.config.js';
 export const invoice = {
 	method: 'GET',
-	path: '/packages/ubc-farm-invoice{ext?}',
+	path: '/packages/ubc-farm-page-invoice{ext?}',
 	handler: {
 		package: Object.assign({}, invoiceConfig, {
-			entry: join(packageFolder, 'ubc-farm-invoice/index.js')
+			entry: join(packageFolder, 'ubc-farm-page-invoice/index.js')
 		})
 	}
 }
