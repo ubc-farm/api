e969fe00209d95581f8549884d23d38f53b44948
Tiger Oakes <tigeroakes@gmail.com>
2016-07-29 11:23:11 -0700

    Added views-server subpackage

---
+++ packages/ubc-farm-views-server/server.js
@@ -0,0 +1,30 @@
+import {Server} from 'hapi';
+import Vision from 'vision';
+import Handlebars from 'handlebars';
+import {resolve} from 'path';
+
+import viewHandler from './view-handler.js'; 
+const packageFolder = resolve(__dirname, '../')
+
+export const port = 3040;
+
+const server = new Server();
+server.connection({port});
+server.path(packageFolder);
+
+server.handler('view', viewHandler);
+server.register(Vision, err => {if (err) throw err});
+
+server.views({
+	engines: {
+		html: Handlebars,
+		hbs: Handlebars
+	},
+	relativeTo: packageFolder,
+	path: '.',
+	partialsPath: 'ubc-farm-view-helpers/partials',
+	helpersPath: 'ubc-farm-view-helpers/helpers',
+	isCached: process.env.NODE_ENV !== 'development'
+})
+
+export default server;
