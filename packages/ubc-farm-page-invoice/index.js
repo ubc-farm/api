71615cc2656cb36ff52d2b25024b548d98aa191a
Tiger Oakes <tigeroakes@gmail.com>
2016-07-29 15:34:12 -0700

    Debugging the invoice package

--- packages/ubc-farm-page-invoice/index.js
+++ packages/ubc-farm-page-invoice/index.js
@@ -4,9 +4,12 @@ import {Provider} from 'react-redux';
 
 import store from './store/index.js';
 import InvoiceTable from './table/main.js';
+import {domready} from '../ubc-farm-utils/index.js';
 
-ReactDOM.render(
-	<Provider store={store}>
-		<InvoiceTable />
-	</Provider>
-)
+domready.then(() => {
+	ReactDOM.render(
+		<Provider store={store}>
+			<InvoiceTable />
+		</Provider>
+	, document.getElementById('invoice-table-node'));
+});
