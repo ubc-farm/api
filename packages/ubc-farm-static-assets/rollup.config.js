6e7aa3f6607591f3e9a45501b9aa9d9b54c49dbc
Tiger Oakes <tigeroakes@gmail.com>
2016-07-29 13:53:14 -0700

    Bugfixing and added prepublish step to master starter

--- packages/ubc-farm-static-assets/rollup.config.js
+++ packages/ubc-farm-static-assets/rollup.config.js
@@ -4,6 +4,7 @@ import json from 'rollup-plugin-json';
 export default {
 	entry: 'index.js',
 	external: ['hapi', 'inert'],
+	plugins: [json()],
 	targets: [
 		{ dest: 'index.node.js', format: 'cjs' },
 		{ dest: 'index.es.js', format: 'es' }
