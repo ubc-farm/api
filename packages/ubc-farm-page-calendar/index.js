502223c4463418c4d408420cfd3a730184017e3e
Tiger Oakes <tigeroakes@gmail.com>
2016-08-02 14:07:35 -0700

    Working on date picker

--- packages/ubc-farm-page-calendar/index.js
+++ packages/ubc-farm-page-calendar/index.js
@@ -1,12 +1,13 @@
-import Overview from './month-overview/connected.js';
-import store from './redux/store.js';
+//import Overview from './month-overview/connected.js';
+//import store from './redux/store.js';
+import DatePicker from './date-picker/container.js';
 import {domready} from '../ubc-farm-utils/index.js';
 import {createElement} from 'react';
 import ReactDOM from 'react-dom';
 
 domready.then(() => {
 	ReactDOM.render(
-		createElement(Overview, {store}),
+		createElement(DatePicker, {}),
 		document.getElementById('app-mount')
 	);
 })
