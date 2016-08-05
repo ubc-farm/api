764a1535b9a82f35a93fde0fca929d9b33917e08
Tiger Oakes <tigeroakes@gmail.com>
2016-08-05 10:29:15 -0700

    Moving timeline logic

--- packages/ubc-farm-page-planner/index.js
+++ packages/ubc-farm-page-planner/index.js
@@ -1,22 +1,9 @@
-import {createElement} from 'react';
+//import {createElement} from 'react';
 import ReactDOM from 'react-dom';
-import Timeline from 'vis-timeline'; //eslint-disable-line 
 import {domready} from '../ubc-farm-utils/index.js';
 //import TaskTile, {Tasks} from './drag-drop/tasklist.js';
+import timelineElement from './drag-drop/index.js';
 
-domready.then(() => {
-	ReactDOM.render(
-		/*createElement('div', {}, 
-			Array.from(Tasks, ([name, color]) => 
-				createElement(TaskTile, {color, name}))
-		),*/
-		createElement(Timeline, {
-			options: {
-				editable: true,
-				min: new Date(new Date().getFullYear() - 1, 0),
-				max: new Date(new Date().getFullYear() + 2, 0, 0)
-			}
-		}),
-		document.getElementById('app-mount')
-	);
-})
+Promise.all([timelineElement, domready]).then(([element]) => {
+	ReactDOM.render(element, document.getElementById('app-mount'))
+});
