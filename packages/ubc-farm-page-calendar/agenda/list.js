6e449105ca9beee7b7602bb7b7c5a54a06ac73b6
Tiger Oakes <tigeroakes@gmail.com>
2016-08-08 15:22:29 -0700

    Added loading animation

--- packages/ubc-farm-page-calendar/agenda/list.js
+++ packages/ubc-farm-page-calendar/agenda/list.js
@@ -1,23 +1,32 @@
 import {createElement as h, PropTypes} from 'react'; /** @jsx h */
 import AgendaItem from './item.js';
 import AgendaDayHeader from './day-header.js';
+import Loader from './loader.js';
 
-const AgendaDayList = ({date, events, onHeaderClick, onItemClick}) => (
+const AgendaDayList = ({date, events, loading, onHeaderClick, onItemClick}) => (
 	<section className='agenda-date-list'>
 		<AgendaDayHeader date={date} onClick={onHeaderClick} />
-		<ol className='agenda-date-list-contents'>
-			{events.map(event => (
-				<AgendaItem {...event} key={event.id}
-					onAction={t => onItemClick(event, t)} 
-				/>
-			))}
-		</ol>
+		{loading 
+			? <Loader />
+			: (
+			<ol className='agenda-date-list-contents'>
+				{events.map(event => (
+					<AgendaItem {...event} key={event.id}
+						onAction={t => onItemClick(event, t)} 
+					/>
+				))}
+			</ol>
+			)
+		}
 	</section>
 )
 
 AgendaDayList.propTypes = {
-	date: PropTypes.instanceOf(Date),
+	date: PropTypes.instanceOf(Date).isRequired,
 	events: PropTypes.arrayOf(PropTypes.object),
+	loading: PropTypes.bool,
 	onHeaderClick: PropTypes.func,
 	onItemClick: PropTypes.func
 }
+
+export default AgendaDayList;
