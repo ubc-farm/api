0479e92e09b7bbc078c296d12956b7e9100d6b6a
Tiger Oakes <tigeroakes@gmail.com>
2016-08-10 17:13:53 -0700

    Updating components

--- packages/ubc-farm-page-map-editor/redux/action-build-grid.js
+++ packages/ubc-farm-page-map-editor/redux/action-build-grid.js
@@ -4,7 +4,8 @@ import map from '../map/map.js';
 import {toGeoJson} from '../map/promisify.js';
 
 import {
-	setSelected, applyGridData, overwriteCells
+	setSelected, setDataLoading,
+	applyGridData, overwriteCells
 } from './actions.js';
 
 const GridWorker = 
@@ -16,12 +17,14 @@ export default function buildGrid(polyID, gridOptions) {
 		if (gridOptions) dispatch(applyGridData(polyID, gridOptions));
 		gridOptions = getState().grids.get(polyID);
 
+		dispatch(setDataLoading(polyID, true));
 		const polygon = map.data.getFeatureById(polyID);
 
 		return toGeoJson(polygon)
 		.then(feature => GridWorker.postMessage({feature, gridOptions}))
 		.then(features => {
 			dispatch(overwriteCells(polyID, features));
+			dispatch(setDataLoading(polyID, false))
 			dispatch(setSelected(polyID));
 		})
 		.catch(err => dispatch(overwriteCells(polyID, undefined, err)));
--- packages/ubc-farm-page-map-editor/toolbar/connected.js
+++ packages/ubc-farm-page-map-editor/toolbar/connected.js
@@ -1,18 +1,16 @@
 import {connect} from 'react-redux';
-import {toggleResize, toggleAdding} from '../redux/actions.js';
+import {toggleResizing, toggleAdding} from '../redux/actions.js';
+import {resizableSelector, addModeSelector} from '../redux/selectors.js';
 
 import ActionToolbar from './with-buttons.js';
 
 export default connect(
-	state => {
-		const gridData = state.gridForm.get(state.active) || {};
-		return {
-			adding: state.mapMeta.adding,
-			resizing: gridData.resizing
-		}
-	},
+	state => ({
+		adding: addModeSelector(state),
+		resizing: resizableSelector(state)
+	}),
 	dispatch => ({
-		onResize: () => dispatch(toggleResize()),
+		onResize: () => dispatch(toggleResizing()),
 		onAdd: () => dispatch(toggleAdding())
 	})
 )(ActionToolbar);
