ec7784fa6fbd6ba50b1194c74a3215671afebefd
Tiger Oakes <tigeroakes@gmail.com>
2016-08-12 09:13:06 -0700

    Updated to redux-form 6

--- packages/ubc-farm-page-directory/editor/form.js
+++ packages/ubc-farm-page-directory/editor/form.js
@@ -1,38 +1,29 @@
-import {createElement as h, PropTypes, Component} from 'react'; /** @jsx h */
-import {reduxForm} from 'redux-form';
+import {createElement as h} from 'react'; /** @jsx h */
+import {reduxForm, Field, propTypes} from 'redux-form';
 
-import Name from './name.js';
-import Email from './email.js';
-import Phone from './phone.js';
-import Address from './address.js';
-
-const Form = ({fields, handleSubmit}) => (
+const Form = ({handleSubmit}) => (
 	<form onSubmit={handleSubmit}>
 		<label>
 			<span className='label-body'>Name</span>
-			<input type='text' {...fields.name} />
+			<Field component='input' type='text' name='name' />
 		</label>
 		<label>
 			<span className='label-body'>Email</span>
-			<input type='email' {...fields.email} />
+			<Field component='input' type='email' name='email' />
 		</label>
 		<label>
 			<span className='label-body'>Phone Number</span>
-			<input type='tel' {...fields.phone} />
+			<Field component='input' type='tel' name='phone' />
 		</label>
 		<div>
 			<label htmlFor='physical-address'>Physical Address</label>
-			<textarea {...fields.addressPhysical} id='physical-address'
-				value={fields.addressPhysical.value || ''} 
-			/>
+			<Field component='textarea' name='addressPhysical' id='addressPhysical' />
 		</div>
 	</form>
 )
 
+Form.propTypes = propTypes;
+
 export default reduxForm({
-	form: 'contact',
-	fields: [
-		'name', 'email', 'phone',
-		'addressPhysical'
-	]
+	form: 'contact'
 })
