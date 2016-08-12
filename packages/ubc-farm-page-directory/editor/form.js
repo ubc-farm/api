32b7a92d1dce02ce152aa65b5666994ec3eade28
Tiger Oakes <tigeroakes@gmail.com>
2016-08-12 09:00:58 -0700

    Working on redux form

--- packages/ubc-farm-page-directory/editor/form.js
+++ packages/ubc-farm-page-directory/editor/form.js
@@ -1,30 +1,38 @@
 import {createElement as h, PropTypes, Component} from 'react'; /** @jsx h */
+import {reduxForm} from 'redux-form';
 
 import Name from './name.js';
 import Email from './email.js';
 import Phone from './phone.js';
 import Address from './address.js';
 
-export default class Form extends Component {
-	static get propTypes() {return {
-		onSubmit: PropTypes.func.isRequired
-	}}
+const Form = ({fields, handleSubmit}) => (
+	<form onSubmit={handleSubmit}>
+		<label>
+			<span className='label-body'>Name</span>
+			<input type='text' {...fields.name} />
+		</label>
+		<label>
+			<span className='label-body'>Email</span>
+			<input type='email' {...fields.email} />
+		</label>
+		<label>
+			<span className='label-body'>Phone Number</span>
+			<input type='tel' {...fields.phone} />
+		</label>
+		<div>
+			<label htmlFor='physical-address'>Physical Address</label>
+			<textarea {...fields.addressPhysical} id='physical-address'
+				value={fields.addressPhysical.value || ''} 
+			/>
+		</div>
+	</form>
+)
 
-	constructor(props) {
-		super(props);
-
-		this.handleTypeChange = this.handleTypeChange.bind(this);
-		this.state = { type: 'Person' };
-	}
-
-	handleTypeChange(type) { this.setState({type}) }
-
-	render() {
-		const {type} = this.state;
-		return (
-			<form /*onSubmit={this.props.onSubmit}*/>
-			
-			</form>
-		)
-	}
-}
+export default reduxForm({
+	form: 'contact',
+	fields: [
+		'name', 'email', 'phone',
+		'addressPhysical'
+	]
+})
