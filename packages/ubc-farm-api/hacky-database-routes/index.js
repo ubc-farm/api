ce50cff0b0fd613f42b6ba1da51a0bc5dada965b
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 10:57:36 -0700

    Disabled validation for hacky routes (for now)

--- packages/ubc-farm-api/hacky-database-routes/index.js
+++ packages/ubc-farm-api/hacky-database-routes/index.js
@@ -8,19 +8,19 @@ import modelHandler from './model-handler/index.js';
 function* methodRoutes(model) {
 	for (let method of methods) {
 		const handler = modelHandler({method}, {model});
-		const config = {validate}
+		//const config = {validate}
 		if (method == 'POST') {
 			yield {
-				method, handler, config,
+				method, handler, //config,
 				path: `/api/${model.label}`,
 			}
 		} else {
 			yield {
-				method, handler, config,
+				method, handler, //config,
 				path: `/api/${model.label}/{id?}`
 			};
 			yield {
-				method, handler, config,
+				method, handler,// config,
 				path: `/api/${model.label}/{id}/{property}`
 			};
 		}
