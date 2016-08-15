b535c4b7aeee5c68f7dbe1b3d8aa7cfc999df23d
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 12:06:18 -0700

    Updated launch.json

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
 	entry: 'index.js',
 	sourceMap: true,
 	targets: [
-		{ dest: 'dist/index.node.js', format: 'cjs' },
+		{ dest: 'dist/index.js', format: 'cjs' },
 		{ dest: 'dist/index.es.js', format: 'es' }
 	],
 	external: [
