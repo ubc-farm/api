c0f9ffa62683b36febce563587f74a53e8c569cd
Tiger Oakes <tigeroakes@gmail.com>
2016-08-24 11:46:05 -0700

    Adding route for getting a single feature

---
+++ server/database-routes/geojson/get-one.js
@@ -0,0 +1,28 @@
+import { notFound } from 'boom';
+import { Field } from '../../../database/index.js';
+import { Feature } from '../../../utils/geojson.js';
+import { transformReply } from '../transformer.js';
+
+/**
+ * Retrieve a single field as a GeoJSON Feature
+ */
+function geojsonSingleFeature(request, reply) {
+	const { id } = request.params;
+
+	const query = Field.query().findById(id)
+		.then(field => {
+			if (field === undefined) return notFound();
+			else if (!field.path) return notFound('No path exists for field');
+
+			const { parent, grid } = field;
+			return new Feature(field.polygon, { parent, grid }, field.$id);
+		});
+
+	return transformReply(query, request, reply);
+}
+
+export default {
+	method: 'GET',
+	path: '/api/fields/geojson/{id}',
+	handler: geojsonSingleFeature,
+};
--- server/database-routes/geojson/index.js
+++ server/database-routes/geojson/index.js
@@ -1,4 +1,5 @@
 import get from './get.js';
+import getOne from './get-one.js';
 import post from './post.js';
 
-export default [get, post];
+export default [get, getOne, post];
